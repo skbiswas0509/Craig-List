766291c2d4562d6d691e26d4f23dff3f974873bc
Sukumar Biswas <skbiswas0509@gmail.com>
2024-07-16 13:36:37 +0600

    Till 5.50 hours

--- my_app/views.py
+++ my_app/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 
 def home(request):
     return render(request, 'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    stuff_for_frontend = {
+        'search':search,
+        }
+    return render(request, 'my_app/new_search.html', stuff_for_frontend)
