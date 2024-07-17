5de7c5096657edae134366cb15b3a57d64d96411
Sukumar Biswas <skbiswas0509@gmail.com>
2024-07-17 11:32:35 +0600

    .

--- my_app/views.py
+++ my_app/views.py
@@ -18,18 +18,28 @@ def new_search(request):
     soup = BeautifulSoup(data, features='html.parser')
 
     post_listings = soup.find_all('a', {'class': 'result_title'})
-
+    BASE_IMAGE_URL = "https://images.craigslist.org/.jpg{}_300*300.jpg"
     final_postings = []
 
     for post in post_listings:
         post_title = post.find(class_='result-title').text
         post_url = post.find('a').get('href')
-        post_price = post.find(class_='result-price').text
+
+        if post.find(class_="result-price"):
+            post_price = post.find(class_='result-price').text
+        else: post_price = "N/A"
     
-    final_postings.append((post_title, post_url, post_price))
+    if post.find(class_='result-image').get('data-ids'):
+        post_image_id = post.find(class_='result-image').get('data-ids')[0].split(',')[0].split[':'][1]
+        post_image_url = BASE_IMAGE_URL.format(post_image_id)
+    else:
+        post_image_url = 'https://craigslist.org/images/peace.org'
 
+    final_postings.append((post_title, post_url, post_price, post_image_url))
 
+   
     stuff_for_frontend = {
         'search':search,
+        'final_postings': final_postings,
         }
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
